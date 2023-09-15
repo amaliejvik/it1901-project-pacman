626a69392ac8865af07bf469707c9f0512a5b1b7
Jenny <jennymul@stud.ntnu.no>
2023-09-15 15:26:13 +0200

    Edit code controller class

--- gr2372/PacMan/src/main/java/app/PacManController.java
+++ gr2372/PacMan/src/main/java/app/PacManController.java
@@ -9,6 +9,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
@@ -36,6 +37,9 @@ public class PacManController {
     @FXML
     private ImageView pacManGif;
 
+    @FXML
+    private AnchorPane mainBackground;
+
     //TIMELINE
 
     Timeline timeline1 = new Timeline(new KeyFrame(Duration.millis(10), new EventHandler<ActionEvent>(){
@@ -54,18 +58,31 @@ public class PacManController {
     @FXML
     private void handleStartButton(){
         try{
-            startbutton.setVisible(false);
-            startScreen.setVisible(false);
-            username.setVisible(false);
-            usernameInput.setVisible(false);
-            pacManText.setVisible(false);
-
+            mainBackground.getChildren().removeAll(startbutton, startScreen,username,usernameInput,pacManText);
         }
         catch (Exception e){
             System.out.println("Could not start animation");
         }
 
     }
+
+    public void gameOver(){
+        timeline1.stop();
+        Rectangle gameoverScreen = new Rectangle(250, 250);
+        Button restartGame = new Button("Restart Game", gameoverScreen);
+        //Legge inn fxml sånn at vi får en game over screen med highscore
+        mainBackground.getChildren().add( gameoverScreen);
+        
+         //lagre score automatisk på brukernavnet som er oppgitt
+        
+    }
+
+    public void restartGame(){
+        handleStartButton();
+        //starte spillet på nytt igjen
+    }
+
+
     private PacMan pacMan;
 
     public PacManController() {
