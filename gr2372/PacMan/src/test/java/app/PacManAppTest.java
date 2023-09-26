5838f74f3dac4ca54a853a2b870c47e5880846c1
amaliejvik <amaligjo@stud.ntnu.no>
2023-09-26 15:00:39 +0200

    A bit improvement on AppTest. PacMan moves now.

--- gr2372/PacMan/src/test/java/app/PacManAppTest.java
+++ gr2372/PacMan/src/test/java/app/PacManAppTest.java
@@ -7,21 +7,45 @@ import org.testfx.framework.junit5.ApplicationTest;
 import static org.testfx.matcher.control.TextInputControlMatchers.hasText;
 import static org.testfx.api.FxAssert.verifyThat;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class PacManAppTest extends ApplicationTest {
 
     private Parent root;
+    private PacManController controller;
 
+    
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("PacMan.fxml"));
         root = fxmlLoader.load();
-        stage.setScene(new Scene(root));
+        this.controller = fxmlLoader.getController();
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setMaximized(false);
         stage.show();
+
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.RIGHT) {
+                    PacMan.changeDirection("RIGHT");
+                } else if (event.getCode() == KeyCode.LEFT) {
+                    PacMan.changeDirection("LEFT");
+                } else if (event.getCode() == KeyCode.DOWN) {
+                    PacMan.changeDirection("DOWN");
+                } else if (event.getCode() == KeyCode.UP) {
+                    PacMan.changeDirection("UP");
+                }
+            }
+        });
 
     }
 
@@ -44,6 +68,30 @@ public class PacManAppTest extends ApplicationTest {
         // Simulate clicking on startbutton
         clickOn("#startButton");
 
+        controller.createAndConfigureTimeline();
+        controller.startTimeline();
+
+        //Simulate moving PacMan to the right
+        press(KeyCode.RIGHT);
+        press(KeyCode.RIGHT);
+        press(KeyCode.RIGHT);
+        press(KeyCode.RIGHT);
+        press(KeyCode.RIGHT);
+        press(KeyCode.RIGHT);
+        press(KeyCode.RIGHT);
+        press(KeyCode.RIGHT);
+        press(KeyCode.RIGHT);
+        press(KeyCode.RIGHT);
+        press(KeyCode.RIGHT);
+        press(KeyCode.RIGHT);
+        press(KeyCode.RIGHT);
+        press(KeyCode.RIGHT);
+        press(KeyCode.RIGHT);
+        press(KeyCode.RIGHT);
+        press(KeyCode.RIGHT);
+        press(KeyCode.RIGHT);
+
+  
     }
 
 }
