0853c9d12d6f5dd0396a2c74050bedf7290f7456
amaliejvik <amaligjo@stud.ntnu.no>
2023-10-18 11:22:40 +0200

    Add PacManAppearence menu
    Player can choose between four different colors on PacMan

--- gr2372/core/src/test/java/core/PacManPersistenceTest.java
+++ gr2372/core/src/test/java/core/PacManPersistenceTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import Persistence.PacmanPersistence;
+import persistence.PacmanPersistence;
 
 public class PacManPersistenceTest {
 
