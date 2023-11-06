4143ccd2d1dfea3d92eb76d2405708f73b567754
HermanOstenby <hermaos@stud.ntnu.no>
2023-11-06 15:04:46 +0100

    Fix syntax errors
    Fixed syntax errors that checkstyle reported as warnings
    
    Related to #50

--- gr2372/core/src/main/java/core/Ghost.java
+++ gr2372/core/src/main/java/core/Ghost.java
@@ -9,84 +9,86 @@ import javafx.scene.shape.Rectangle;
  */
 public class Ghost {
 
-    private double dx = 0;
-    private double dy = 0;
-    private double xPosition;
-    private double yPosition;
-    
-    /*
-     * Constructor for a ghost-object
-     */
-    public Ghost() {
-        return;
-    }
+  private double dx = 0;
+  private double dy = 0;
+  private double xposition;
+  private double yposition;
+  
+  /*
+   * Constructor for a ghost-object
+   */
+  public Ghost() {
+    return;
+  }
 
-    //Various getters and setters
+  //Various getters and setters
 
-    public double getXPosition() {
-        return xPosition;
-    }
+  public double getXposition() {
+    return xposition;
+  }
 
-    public void setXPosition(double x) {
-        xPosition = x;
-    }
+  public void setXposition(double x) {
+    xposition = x;
+  }
 
-    public double getYPosition() {
-        return yPosition;
-    }
+  public double getYposition() {
+    return yposition;
+  }
 
-    public void setYPosition(double y) {
-        yPosition = y;
-    }
+  public void setYposition(double y) {
+    yposition = y;
+  }
 
-    public double getDX() {
-        return dx;
-    }
-    
-    public void setDX(double x) {
-        dx = x;
-    }
+  public double getDx() {
+    return dx;
+  }
+  
+  public void setDx(double x) {
+    dx = x;
+  }
 
-    public double getDY() {
-        return dy;
-    }
+  public double getDy() {
+    return dy;
+  }
 
-    public void setDY(double y) {
-        dy = y;
-    }
+  public void setDy(double y) {
+    dy = y;
+  }
 
-    /**
-     * Changes the ghosts direction of travel.
-     * Sets the variables dx and dy which are read by the timeline function in the controller.
-     * @param string the direction of the corresponding arrow key typed.
-     */
-    public void changeDirection(String string) {
-        if (string.equals("RIGHT")) {
-            dy = 0;
-            dx = 1;
-        } else if (string.equals("LEFT")) {
-            dy = 0;
-            dx = -1;
-        } else if (string.equals("UP")) {
-            dy = -1;
-            dx = 0;
-        } else if (string.equals("DOWN")) {
-            dy = 1;
-            dx = 0;
-        }
-    }
+  /**
+   * Changes the ghosts direction of travel.
+   * Sets the variables dx and dy which are read by the timeline function in the controller.
 
-    /**
-     * Checks if ghost collides with a wall
-     * Ghosts position bounces back 3px from the wall to ensure it does not stop in a wall.
-     * @param ghost pacman-image
-     * @param wall wall to check collision with
-     * @return boolean if ghost has collided with wall, false if no collision
-     */
-    public boolean checkWallCollision(ImageView ghost, Rectangle wall) {
-        if (ghost.getBoundsInParent().intersects(wall.getBoundsInParent())) {
-            return true;
-            }
-        return false;
+   * @param string the direction of the corresponding arrow key typed.
+   */
+  public void changeDirection(String string) {
+    if (string.equals("RIGHT")) {
+      dy = 0;
+      dx = 1;
+    } else if (string.equals("LEFT")) {
+      dy = 0;
+      dx = -1;
+    } else if (string.equals("UP")) {
+      dy = -1;
+      dx = 0;
+    } else if (string.equals("DOWN")) {
+      dy = 1;
+      dx = 0;
     }
+  }
+
+  /**
+   * Checks if ghost collides with a wall
+   * Ghosts position bounces back 3px from the wall to ensure it does not stop in a wall.
+
+   * @param ghost pacman-image
+   * @param wall wall to check collision with
+   * @return boolean if ghost has collided with wall, false if no collision
+   */
+  public boolean checkWallCollision(ImageView ghost, Rectangle wall) {
+    if (ghost.getBoundsInParent().intersects(wall.getBoundsInParent())) {
+      return true;
+    }
+    return false;
+  }
 }
--- gr2372/core/src/main/java/core/PacMan.java
+++ gr2372/core/src/main/java/core/PacMan.java
@@ -8,113 +8,110 @@ package core;
  */
 public class PacMan {
 
-    private static double dx = 0;
-    private static double dy = 0;
-    private static double rotate;
-    private static double xPosition = 330;
-    private static double yPosition = 115;
+  private static double dx = 0;
+  private static double dy = 0;
+  private static double rotate;
+  private static double xPosition = 330;
+  private static double yPosition = 115;
 
-    // Various getters and setters
+  // Various getters and setters
 
-    public static double getXPosition() {
-        return xPosition;
+  public static double getXposition() {
+    return xPosition;
+  }
+
+  public static void setXposition(double x) {
+    xPosition = x;
+  }
+
+  public static double getYposition() {
+    return yPosition;
+  }
+
+  public static void setYposition(double y) {
+    yPosition = y;
+  }
+
+  public static double getDx() {
+    return dx;
+  }
+
+  public static double getDy() {
+    return dy;
+  }
+
+  public static double getRotate() {
+    return rotate;
+  }
+
+  public static void setDx(double x) {
+    dx = x;
+  }
+
+  public static void setDy(double y) {
+    dy = y;
+  }
+
+  public static void setRotate(double r) {
+    rotate = r;
+  }
+
+  /**
+   * Resets Pacman to start position.
+   */
+  public static void reset() {
+    setDx(0);
+    setDy(0);
+    setRotate(0);
+    setXposition(330);
+    setYposition(115);
+  }
+
+  /**
+   * Changes PacMan's direction of travel and rotation.
+   * Receives key inputs from PacManApp.java.
+   * Sets the static variables dx, dy and rotation which are read by the timeline
+   * function in the controller.
+
+   * @param string the direction of the corresponding arrow key typed.
+   */
+  public static void changeDirection(String string) {
+    if (string.equals("RIGHT")) {
+      dy = 0;
+      dx = 1;
+      rotate = 0;
+    } else if (string.equals("LEFT")) {
+      dy = 0;
+      dx = -1;
+      rotate = 1;
+    } else if (string.equals("UP")) {
+      dy = -1;
+      dx = 0;
+      rotate = 2;
+    } else if (string.equals("DOWN")) {
+      dy = 1;
+      dx = 0;
+      rotate = 3;
     }
+  }
 
-    public static void setXPosition(double x) {
-        xPosition = x;
-    }
+  /**
+   * Translates rotation-index to degrees.
+   * 0 -> right,
+   * 1 -> left,
+   * 2 -> up,
+   * 3 -> down.
 
-    public static double getYPosition() {
-        return yPosition;
+   * @return correct rotation angle corresponding to direction of travel
+   */
+  public static double rotationAngle() {
+    if (rotate == 0) {  // RIGHT
+      return 0;
+    } else if (rotate == 1) {   //LEFT
+      return 180;
+    } else if (rotate == 2) {   // UP
+      return 270;
     }
-
-    public static void setYPosition(double y) {
-        yPosition = y;
-    }
-
-    public static double getDX() {
-        return dx;
-    }
-
-    public static double getDY() {
-        return dy;
-    }
-
-    public static double getRotate() {
-        return rotate;
-    }
-
-    public static void setDX(double x) {
-        dx = x;
-    }
-
-    public static void setDY(double y) {
-        dy = y;
-    }
-
-    public static void setRotate(double r) {
-        rotate = r;
-    }
-
-    public static void reset() {
-        setDX(0);
-        setDY(0);
-        setRotate(0);
-        setXPosition(330);
-        setYPosition(115);
-    }
-
-    /**
-     * Changes PacMan's direction of travel and rotation.
-     * Receives key inputs from PacManApp.java.
-     * Sets the static variables dx, dy and rotation which are read by the timeline
-     * function in the controller.
-     * 
-     * @param string the direction of the corresponding arrow key typed.
-     */
-    public static void changeDirection(String string) {
-        if (string.equals("RIGHT")) {
-            dy = 0;
-            dx = 1;
-            rotate = 0;
-        } else if (string.equals("LEFT")) {
-            dy = 0;
-            dx = -1;
-            rotate = 1;
-        } else if (string.equals("UP")) {
-            dy = -1;
-            dx = 0;
-            rotate = 2;
-        } else if (string.equals("DOWN")) {
-            dy = 1;
-            dx = 0;
-            rotate = 3;
-        }
-    }
-
-    /**
-     * Translates rotation-index to degrees.
-     * 0 -> right,
-     * 1 -> left,
-     * 2 -> up,
-     * 3 -> down.
-     * 
-     * @return correct rotation angle corresponding to direction of travel
-     */
-    public static double rotationAngle() {
-        // RIGHT
-        if (rotate == 0) {
-            return 0;
-        }
-        // LEFT
-        else if (rotate == 1) {
-            return 180;
-        }
-        // UP
-        else if (rotate == 2) {
-            return 270;
-        }
-        // DOWN
-        return 90;
-    }
+    return 90;  // DOWN
+  }
 }
